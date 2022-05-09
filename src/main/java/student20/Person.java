5052cb636589ccc514d028fe39221ce9c36da54d
Elden-Lord01 <levi.melton43@gmail.com>
2022-05-09 15:16:43 -0400

    Ex 1 & 2

---
+++ src/main/java/student20/Person.java
@@ -0,0 +1,31 @@
+package student20;
+
+import java.util.Objects;
+
+public class Person {
+
+    String fName;
+    String lName;
+    int age;
+    String SSN;
+
+    Person(String fName, String lName, int age, String SSN) {
+        this.fName = fName;
+        this.lName = lName;
+        this.age = age;
+        this.SSN = SSN;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return SSN.equals(person.SSN);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(SSN);
+    }
+}
