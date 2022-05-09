315ca20b983d31c01247b5c588693f8aa5fc186c
Donna Lemon <donnalemon1@gmail.com>
2022-05-09 17:20:02 -0400

    Ex. 2.

--- src/main/java/student9/Main.java
+++ src/main/java/student9/Main.java
@@ -5,64 +5,52 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        ex1();
-        //ex2();
+        //ex1();
+        ex2();
         //ex3();
         //ex4();
         //ex5();
         //ex6();
     }
 
-    public Main() {
-    }
-
-    private static void ex1() {
-
-        var list = new ArrayList<Person>(); // p, p1, p2.
-
-        var personMap = new HashMap<String, Integer>();
-
-        var p = new Person("Tom", "Thumb", 11, "123-45-1234");
-        var p1 = new Person("Jon", "Smith", 22, "123-45-1234");
-        var p2 = new Person("Jon", "Smith", 22, "000-00-0000");
-
-        if (p.equals(p1)) {
-            System.out.printf("Yes");
-        }
-        if (p1.equals(p2)) {
-            System.out.println("Here");
-        }
-     /*   Output:
-```
-        Yes
-```
-
-        //The `Person`class overrides the equals method.
-            //System.out.println("TODO...");*/
-    }
-    /*Ex. 1 Equals
-
-    Create a `Person` class that can be consumed in the following manner:
-
-    ```java */
+//    private static void ex1() {
+//        var p = new Person("Tom", "Thumb", 11, "123-45-1234");
+//        var p1 = new Person("Jon", "Smith", 22, "123-45-1234");
+//        var p2 = new Person("Jon", "Smith", 22, "000-00-0000");
+//
+//        if (p.equals(p1)) {
+//            System.out.printf("Yes");
+//        }
+//        if (p1.equals(p2)) {
+//            System.out.println("Here");
+//        }
+//        System.out.println("TODO...");
+//    }
 
     private static void ex2() {
-        System.out.println("TODO...");
-    }
+        var inputString = "The water main broke on water street on the day before my birthday";
+        var wordCounter = new WordCounter(inputString);
+        wordCounter.countWords();
+        System.out.println(wordCounter.getWordCount());
 
-    private static void ex3() {
-        System.out.println("TODO...");
-    }
 
-    private static void ex4() {
-        System.out.println("TODO...");
-    }
+//        System.out.println("TODO...");
+//    }
+//
+//    private static void ex3() {
+//        System.out.println("TODO...");
+//    }
+//
+//    private static void ex4() {
+//        System.out.println("TODO...");
+//    }
+//
+//    private static void ex5() {
+//        System.out.println("TODO...");
+//    }
+//
+//    private static void ex6() {
+//        System.out.println("TODO...");
+//    }
+    }}
 
-    private static void ex5() {
-        System.out.println("TODO...");
-    }
-
-    private static void ex6() {
-        System.out.println("TODO...");
-    }
-}
