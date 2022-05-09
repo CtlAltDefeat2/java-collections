9167762b38ed050e271e223420e285f5e2f37d1a
Brianprice098 <103675908+Brianprice098@users.noreply.github.com>
2022-05-09 10:59:35 -0400

    Ex 1 complete

--- src/main/java/student3/Main.java
+++ src/main/java/student3/Main.java
@@ -1,4 +1,5 @@
 package student3;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +13,17 @@ public class Main {
 
     private static void ex1() {
         System.out.println("TODO...");
+        var p = new Person("Tom", "Thumb", 11, "123-45-1234");
+        var p1 = new Person("Jon", "Smith", 22, "123-45-1234");
+        var p2 = new Person("Jon", "Smith", 22, "000-00-0000");
+
+
+        if (p.equals(p1)) {
+            System.out.printf("Yes");
+        }
+        if (p1.equals(p2)) {
+            System.out.println("Here");
+        }
     }
 
     private static void ex2() {
---
+++ src/main/java/student3/Person.java
@@ -0,0 +1,32 @@
+package student3;
+
+import javax.lang.model.element.VariableElement;
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String ssn;
+
+    public Person(String firstName, String lastName, int age, String ssn) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.ssn = ssn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return ssn.equals(person.ssn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ssn);
+    }
+}
