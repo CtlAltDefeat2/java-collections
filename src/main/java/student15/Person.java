76ed8aed30ad6f735c020934fac6bc6d23c26b8e
PKavya1792 <103611194+PKavya1792@users.noreply.github.com>
2022-05-09 12:31:25 -0400

    try

---
+++ src/main/java/student15/Person.java
@@ -0,0 +1,18 @@
+package student15;
+
+public class Person {
+
+    private int age;
+    private String name;
+    private String ssn;
+
+    // Constructor
+    public Person(int age, String name, String ssn) {
+        this.age = age;
+        this.name = name;
+        this.ssn = ssn;
+    }
+
+
+
+}
