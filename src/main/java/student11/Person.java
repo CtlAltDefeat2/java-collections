00da1abbbed8db1a58231bcd61f458c70935f770
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-05-09 15:12:11 -0400

    ex1

--- src/main/java/student11/Person.java
+++ src/main/java/student11/Person.java
@@ -1,4 +1,36 @@
 package student11;
 
+import java.util.Objects;
+
 public class Person {
+    //
+    //Data Members
+    //
+    private String name;
+    private String lastName;
+    private int age;
+    private String ssn;
+
+    //
+    //Constructors
+    //
+    public Person(String name, String lastName, int age, String ssn) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.ssn = ssn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return ssn.equals(person.ssn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ssn);
+    }
 }
