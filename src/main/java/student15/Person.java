2c7ef6819bccb3d87ccbaab067fad52431dfc5e6
PKavya1792 <103611194+PKavya1792@users.noreply.github.com>
2022-05-09 15:57:08 -0400

    Ex1

--- src/main/java/student15/Person.java
+++ src/main/java/student15/Person.java
@@ -1,18 +1,36 @@
 package student15;
 
+import java.util.Objects;
+
 public class Person {
 
-    private int age;
     private String name;
+    private String lastName;
+    private int age;
     private String ssn;
 
     // Constructor
-    public Person(int age, String name, String ssn) {
-        this.age = age;
+
+    public Person(String name, String lastName, int age, String ssn) {
         this.name = name;
+        this.lastName = lastName;
+        this.age = age;
         this.ssn = ssn;
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return Objects.equals(ssn, person.ssn);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(ssn);
+    }
 }
+
+
