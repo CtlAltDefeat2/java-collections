6f88a351cf2250ff5927280124544ee66b06256e
TejPatel11 <tejpatel798@gmail.com>
2022-05-09 11:07:23 -0400

    EX1.

---
+++ src/main/java/student24/Person.java
@@ -0,0 +1,27 @@
+package student24;
+
+import java.util.Objects;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String ssn;
+
+    public Person(String firstName, String lastName, int age, String ssn) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.ssn = ssn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return ssn.equals(person.ssn);
+    }
+
+
+}
