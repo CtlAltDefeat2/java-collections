506700351385bf84b383f6b1d9867dac0c5fe307
Micheal Moore <mmoore@xpanxion.com>
2022-05-09 11:09:34 -0400

    Refractored Ex 2.

--- src/main/java/student17/WordCounter.java
+++ src/main/java/student17/WordCounter.java
@@ -7,17 +7,11 @@ public class WordCounter {
     //
     //Data Members
     //
-    private String sent;
-
     Map<String, Integer> hashMap = new HashMap<>();
-
     //
     //Constructor
     //
-
-
     public WordCounter(String sent) {
-        this.sent = sent;
         String[] words = sent.split(" ");
         for(String word : words){
             hashMap.merge(word, 1, Integer::sum);
@@ -26,16 +20,9 @@ public class WordCounter {
             System.out.println(entry.getKey() + " " + entry.getValue());
         });
     }
-
     public void countWords() {
     }
-
-    public boolean getWordCount() {
-        return false;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
+    public String getWordCount() {
+        return " ";
     }
 }
