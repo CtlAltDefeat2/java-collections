af0b356d3bbada529b4289d1c2b83a7e796b94b4
Shilpa Sivarajan <shilpa.vchand@gmail.com>
2022-05-09 12:13:08 -0400

    ex1

---
+++ src/main/java/student23/Person.java
@@ -0,0 +1,31 @@
+package student23;
+
+import java.util.Objects;
+
+public class Person {
+
+    String FirstName = "";
+    String LastName = "";
+    int age = 0;
+    String ssn ="";
+
+    public Person(String firstName, String lastName, int age, String ssn) {
+        FirstName = firstName;
+        LastName = lastName;
+        this.age = age;
+        this.ssn = ssn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && FirstName.equals(person.FirstName) && LastName.equals(person.LastName) && ssn.equals(person.ssn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(FirstName, LastName, age, ssn);
+    }
+}
