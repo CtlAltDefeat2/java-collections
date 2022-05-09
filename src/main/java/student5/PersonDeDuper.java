ba299bbb12f531989348fb04f832770db98ab262
Connor Shillady <c.shillady24@gmail.com>
2022-05-09 19:51:16 -0400

    Setup for Exercises 3 and 4

---
+++ src/main/java/student5/PersonDeDuper.java
@@ -0,0 +1,17 @@
+package student5;
+
+public class PersonDeDuper {
+    // Variable Declarations
+    // Constructors
+    public PersonDeDuper () {}
+    // Accessors
+    // Methods
+    public void addPerson(Person person) {
+        //
+    }
+    public String getUniquePeople() {
+        //
+        return "";
+    }
+    // Overrides
+}
---
+++ src/main/java/student5/WordReverser.java
@@ -0,0 +1,20 @@
+package student5;
+
+public class WordReverser {
+    // Variable Declarations
+    private String input;
+    // Constructors
+    public WordReverser (String input) {
+        this.input = input;
+    }
+    // Accessors
+    // Methods
+    public void reverseWords() {
+        //
+    }
+    public String getReversedWords() {
+        //
+        return "";
+    }
+    // Overrides
+}
