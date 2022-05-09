c5d96bdc63e22005978cdecc1168f2eff3bea51f
Elden-Lord01 <levi.melton43@gmail.com>
2022-05-09 15:21:13 -0400

    Update

--- src/main/java/student20/Main.java
+++ src/main/java/student20/Main.java
@@ -11,9 +11,9 @@ public class Main {
     }
 
     private static void ex1() {
-        var p = new Person("Tom", "Thumb", 11, "123-45-1234");
-        var p1 = new Person("Jon", "Smith", 22, "123-45-1234");
-        var p2 = new Person("Jon", "Smith", 22, "000-00-0000");
+        var p = new Person("Alexis", "Maddison", 11, "123-45-1234");
+        var p1 = new Person("Tala", "Domon", 22, "123-45-1234");
+        var p2 = new Person("Solstice", "Salem", 22, "000-00-0000");
 
         if (p.equals(p1)) {
             System.out.printf("Yes");
@@ -24,7 +24,7 @@ public class Main {
     }
 
     private static void ex2() {
-        var inputString = "The water main broke on water street on the day before my birthday";
+        var inputString = "I own two dogs and Four cats.";
         var wordCounter = new WordCounter(inputString);
         wordCounter.countWords();
         System.out.println(wordCounter.getWordCount());
