24c21982a982247943f524d0f2c168280ae5d377
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-05-09 12:27:10 -0400

    Initial Push

--- src/main/java/student11/Main.java
+++ src/main/java/student11/Main.java
@@ -2,7 +2,7 @@ package student11;
 
 public class Main {
     public static void main(String[] args) {
-        ex1();
+          ex1();
         //ex2();
         //ex3();
         //ex4();
@@ -11,26 +11,37 @@ public class Main {
     }
 
     private static void ex1() {
-        System.out.println("TODO...");
-    }
+//        var p = new Person("Tom", "Thumb", 11, "123-45-1234");
+//        var p1 = new Person("Jon", "Smith", 22, "123-45-1234");
+//        var p2 = new Person("Jon", "Smith", 22, "000-00-0000");
+//
+//        if (p.equals(p1)) {
+//            System.out.printf("Yes");
+//        }
+//        if (p1.equals(p2)) {
+//            System.out.println("Here");
+//        }
+//        System.out.println("TODO...");
+//    }
 
-    private static void ex2() {
-        System.out.println("TODO...");
-    }
-
-    private static void ex3() {
-        System.out.println("TODO...");
-    }
-
-    private static void ex4() {
-        System.out.println("TODO...");
-    }
-
-    private static void ex5() {
-        System.out.println("TODO...");
-    }
-
-    private static void ex6() {
-        System.out.println("TODO...");
+//    private static void ex2() {
+//        System.out.println("TODO...");
+//    }
+//
+//    private static void ex3() {
+//        System.out.println("TODO...");
+//    }
+//
+//    private static void ex4() {
+//        System.out.println("TODO...");
+//    }
+//
+//    private static void ex5() {
+//        System.out.println("TODO...");
+//    }
+//
+//    private static void ex6() {
+//        System.out.println("TODO...");
+//    }
     }
 }
---
+++ src/main/java/student11/Person.java
@@ -0,0 +1,4 @@
+package student11;
+
+public class Person {
+}
