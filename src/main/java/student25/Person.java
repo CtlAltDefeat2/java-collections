c5610a178dcbcbb91553c719c08e77c665adb8bb
wdelacruzl <wdelacruzl98@gmail.com>
2022-05-09 11:08:06 -0400

    Ex 1 done

---
+++ src/main/java/student25/Person.java
@@ -0,0 +1,26 @@
+package student25;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private String lastName;
+    private Integer age;
+    private String ssn;
+
+    public Person(String name, String lastName, Integer age, String ssn) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.ssn = ssn;
+    }
+    //The Person class overrides the equals' method.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return ssn == person.ssn;
+    }
+
+}
