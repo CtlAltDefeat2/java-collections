72247c72dcb19bbb7b43aa3ae1ececc6353b3c84
Shilpa Sivarajan <shilpa.vchand@gmail.com>
2022-05-09 15:55:22 -0400

    ex2

--- src/main/java/student23/WordCounter.java
+++ src/main/java/student23/WordCounter.java
@@ -1,21 +1,40 @@
 package student23;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class WordCounter {
     StringTokenizer stringTokenizer;
-    int wordCount = 0;
+    HashMap<String, Integer> hashmap = new HashMap<>();
+    Map.Entry<String, Integer> entry;
 
     public WordCounter(String inputString) {
         stringTokenizer = new StringTokenizer(inputString);
     }
 
     public void countWords() {
-      wordCount = stringTokenizer.countTokens();
+        int value = 0;
 
+        while(stringTokenizer.hasMoreTokens()){
+            String key = stringTokenizer.nextToken().toLowerCase();
+
+            if(hashmap.containsKey(key)){
+                value = hashmap.get(key);
+                value++;
+                hashmap.replace(key, value);
+            }
+            else{
+                hashmap.put(key , 1);
+            }
+        }
     }
 
-    public int getWordCount() {
-        return wordCount;
+    public String getWordCount() {
+        String output = "";
+        for(Map.Entry<String, Integer> entry : hashmap.entrySet()){
+            output += entry.getKey() + " " + entry.getValue() + "\n";
+        }
+        return output;
     }
 }
