332b77ea2222604988d472e045ad8b33c3cd27a0
Michael Leahey <michael.leahey1843@outlook.com>
2022-05-09 11:05:04 -0400

    update

---
+++ src/main/java/student16/Person.java
@@ -0,0 +1,13 @@
+package student16;
+
+public class Person {
+    private int person;
+    private int person1;
+    private int person2;
+
+    public Person(int person, int person1, int person2) {
+        this.person = person;
+        this.person1 = person1;
+        this.person2 = person2;
+    }
+}
