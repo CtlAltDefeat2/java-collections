ba0927ba0e873c3a5eb8588852ebe1f77195be77
Connor Shillady <c.shillady24@gmail.com>
2022-05-09 11:39:05 -0400

    Exercise 1

---
+++ src/main/java/student5/Person.java
@@ -0,0 +1,37 @@
+package student5;
+
+public class Person {
+    // Variable Declarations
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String ssn;
+    // Constructors
+    public Person (String firstName, String lastName, int age, String ssn) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.ssn = ssn;
+    }
+    // Methods
+    public String getSsn() {
+        return ssn;
+    }
+    // Overrides
+    @Override
+    public boolean equals(Object object) {
+        boolean isItTrue = false;
+        if (this == object) {
+            isItTrue = true;
+        }
+        if (object == null) {
+            isItTrue = false;
+        }
+        if (object instanceof Person) {
+            Person person = (Person) object;
+            if (person.ssn == this.ssn) {
+                isItTrue = true;
+            }
+        } return isItTrue;
+    }
+}
