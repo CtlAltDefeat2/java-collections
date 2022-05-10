413a3cce20a8ff9ce25075fd1b682e8a87808e41
Birk <rgeebt01@yahoo.com>
2022-05-10 14:57:36 -0400

    finished 4

--- src/main/java/student2/WordReverser.java
+++ src/main/java/student2/WordReverser.java
@@ -1,4 +1,32 @@
 package student2;
 
+import java.util.Stack;
+
 public class WordReverser {
+    private String str;
+    private Integer count=0;
+    private String reverseStack="";
+    Stack<String> stack=new Stack<>();
+    public WordReverser(String str) {
+        this.str=str;
+    }
+
+    public void reverseWords() {
+        String[] token=str.split(" ");
+        for (int i=0; i< token.length;i++){
+            this.stack.push(token[i]);
+        }
+        while (!stack.empty()) {
+            reverseStack+=stack.pop()+" ";
+            count++;
+        }
+    }
+
+    public String getReversedWords() {
+
+        return reverseStack;
+    }
+    public Integer getWordCount(){
+        return count;
+    }
 }
