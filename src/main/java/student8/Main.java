96df77a9dbefb2a3b366ecceee692fcd7083c8e2
DominicDiMarcello <103143836+DominicDiMarcello@users.noreply.github.com>
2022-05-10 11:57:38 -0400

    Ex 3

--- src/main/java/student8/Main.java
+++ src/main/java/student8/Main.java
@@ -3,8 +3,8 @@ package student8;
 public class Main {
     public static void main(String[] args) {
         //ex1();
-        ex2();
-        //ex3();
+        //ex2();
+        ex3();
         //ex4();
         //ex5();
         //ex6();
@@ -31,7 +31,18 @@ public class Main {
     }
 
     private static void ex3() {
-        System.out.println("TODO...");
+        var p = new Person("Alice", "Jones", 11, "111-11-1111");
+        var p1 = new Person("Alice", "Jones", 11, "111-11-1111");
+        var p2 = new Person("Bob", "Smith", 22, "222-22-2222");
+        var p3 = new Person("Bob", "Smith", 22, "222-22-2222");
+
+        var personDeDuper = new PersonDeDuper();
+        personDeDuper.addPerson(p);
+        personDeDuper.addPerson(p1);
+        personDeDuper.addPerson(p2);
+        personDeDuper.addPerson(p3);
+
+        System.out.println(personDeDuper.getUniquePeople());
     }
 
     private static void ex4() {
